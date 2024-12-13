c8eaf7a232877c32d0acd8052a19480a598630cd
mpdsant <michael.santos@embraer.com.br>
2024-12-13 13:54:48 -0300

    first commit

---
+++ src/main/java/com/lanziloti/lz_api/LzApiApplication.java
@@ -0,0 +1,13 @@
+package com.lanziloti.lz_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LzApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lanziloti/lz_api/LzApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanziloti.lz_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LzApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
